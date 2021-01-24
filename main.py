dc18aa706dfe6b15069ca9f4cacec58f4c69e6b3
Pezhman Kasraee <me@pezhmankasraee.com>
2021-01-24 16:43:25 +0100

    add fetch area information from traffic service
    tracking-id: US2108:T002

--- main.py
+++ main.py
@@ -29,8 +29,36 @@ def fetch_from_traffic_service():
         try:
             areas_json_object = fetch_area_data()
             save_area_data_in_temp_files(areas_json_object)
+
+            messages_json_object = fetch_messages_data()
+            save_messages_data_in_temp_files(messages_json_object)
         except Exception as e:
             logging.error('Exception in thread: {0}'.format(str(e)))
+
+
+def fetch_messages_data():
+    try:
+        response_message = requests.get('http://api.sr.se/api/v2/traffic/messages?format=json&indent=true')
+        response_message_json = response_message.json()
+        messages = response_message_json['messages']
+        messages_json_object = {'messages': messages}
+        return messages_json_object
+    except Exception as e:
+        logging.error('Exception in thread: {0}'.format(str(e)))
+
+
+def save_messages_data_in_temp_files(messages_json_object):
+    try:
+        with open('data/temp/messages.json', 'w') as file:
+            file.write(str(messages_json_object))
+        with open('data/temp/messages.json.txt', 'w') as file:
+            messages_json_string = json.dumps(messages_json_object, indent=2)
+            file.write(messages_json_string)
+        logging.info('All MESSAGEs are fetched and saved successfully')
+    except IOError as ioe:
+        logging.error('IOException in thread [message]: {0}'.format(str(ioe)))
+    except Exception as e:
+        logging.error('Exception in thread [message]: {0}'.format(str(e)))
 
 
 def fetch_area_data():
@@ -72,6 +100,10 @@ def main():
         try:
             os.remove('data/temp/areas.json')
             os.remove('data/temp/areas.json.txt')
+
+            os.remove('data/temp/messages.json')
+            os.remove('data/temp/messages.json.txt')
+
             logging.info("Server was shut down")
         except Exception as e:
             logging.error('Exception writing areas.json.txt. {0}'.format(str(e)))
