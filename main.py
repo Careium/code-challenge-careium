5862fb297f6733b7eb44a89e93a97952112763c2
Pezhman Kasraee <me@pezhmankasraee.com>
2021-01-24 14:36:02 +0100

    add log to server
    	- read log config file
    	- add a log to server
    
    tracking-id: US2105:T002

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import logging.config
+
+
+logging.config.fileConfig('resources/logging.config')
+logger = logging.getLogger('simpleExample')
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 def server():
+    logging.info("Server starting ...")
     app.run(port=8090)
 
 
